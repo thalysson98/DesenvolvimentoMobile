775f3587a1c0b21d7e5a0a4693d0b755b72ca065
Thalysson <thalyssonmarcelino98@gmail.com>
2026-09-03 20:16:35 -0300

    inicio

--- App.js
+++ App.js
@@ -1,64 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import { Button } from 'react-native-paper';
-import { useState } from 'react';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./src/views/Home";
+import Movie from "./src/views/Movies";
+import Movies from "./src/views/Movies";
 
-import Count from './components/count';
-import CardComponent from './components/CardComponent';
 
 export default function App() {
-
-
+  const Stack = createStackNavigator();
   return (
-    <ScrollView style={styles.container}>
-      {/* <Count/> */}
-      <CardComponent
-      img = "https://hortifrutibr.vtexassets.com/arquivos/ids/173269/batata-inglesa-unidade.jpg?v=638887886306900000"
-      title = "Tababa"
-      text = "Uma batata"
-      />
-      <CardComponent
-      img = "https://hiperideal.vtexassets.com/arquivos/ids/167688/47058.jpg?v=636615816233100000"
-      title = "Necoura"
-      text = "Uma cenoura"
-      />
-      <CardComponent
-      img = "https://hortifrutibr.vtexassets.com/arquivos/ids/173069/140279---2116390000006---alface-crespa-verde-hidrop-un.png?v=638889862302330000"
-      title = "Afalce"
-      text = "Um alface"
-      />
-    
-      <StatusBar style="auto" />
-    </ScrollView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HomeScreen" component={Home}></Stack.Screen>
+        <Stack.Screen name="MoviesScreen" component={Movies}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#b99f9fff',
-  },
-
-  txt: {
-    color: '#fff',
-    fontSize: 18,
-    marginBottom: 5,
-  },
-
-  txt_input: {
-    borderWidth: 1,
-    borderColor: '#b98989',
-    borderRadius: 6,
-    padding: 10,
-    color: '#fff',
-    outlineStyle: 'none',
-    width: 250,
-    marginBottom: 15,
-  },
-
-  btn: {
-    marginTop: 10,
-    width: 250,
-    backgroundColor: '#7a4747ff',
-  },
-});
---
+++ src/views/Home.js
@@ -0,0 +1,28 @@
+import { View,Text,StyleSheet } from "react-native";
+import { Button } from 'react-native-paper';
+
+export default function Home({navigation}) {
+  return (
+    <View style = {styles.container}>
+        <Text style={styles.text}>
+            Home
+        </Text>
+         <Button
+            onPress={()=>navigation.navigate('MoviesScreen')} 
+            mode="contained">
+            Entrar
+        </Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:"center",
+        alignItems: "center"
+    },
+    text:{
+        fontSize:20,
+    }
+})
